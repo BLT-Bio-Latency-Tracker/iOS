43aefa1177d6331b4eee2f46839cdeef95db0ba0
= <leeh4743@naver.com>
2026-04-27 21:15:28 +0900

    Initial Commit

---
+++ BLT/BLTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLTApp.swift
+//  BLT
+//
+//  Created by 신찬솔 on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BLTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLTTests/BLTTests.swift
@@ -0,0 +1,17 @@
+//
+//  BLTTests.swift
+//  BLTTests
+//
+//  Created by 신찬솔 on 4/27/26.
+//
+
+import Testing
+@testable import BLT
+
+struct BLTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
