f7ba328fa8f10a89bb3675ecc35ce82ae38a6d52
= <leeh4743@naver.com>
2026-05-06 16:52:15 +0900

    chore(frontend): 프로젝트 초기 세팅 정리

---
+++ BLT/App/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  BLT
+//
+//  Created by 신찬솔 on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Bio-Latency Tracker")
+    }
+}
