c6f6760d0351289a2c14c44c5b7b0af2a5523416
= <leeh4743@naver.com>
2026-06-18 20:54:25 +0900

    feat(frontend): add firebase messaging setup

--- BLT/App/BLTApp.swift
+++ BLT/App/BLTApp.swift
@@ -5,10 +5,15 @@
 //  Created by 신찬솔 on 4/27/26.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct BLTApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
